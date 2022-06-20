fa99c4dfa874b6497e8e7afcb6c3bb9047ace041
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-06-20 17:32:49 -0400

    Setup navigation

---
+++ src/screens/AllExpensesScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const AllExpensesScreen = () => {
+    return (
+        <View>
+            <Text>All Expenses</Text>
+        </View>
+    );
+};
+
+export default AllExpensesScreen;
